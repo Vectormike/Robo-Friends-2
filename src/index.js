4c6e444ea4abc0ccf63fdd9a485783444f32c55b
Vectormike40 <victor.junior40@yahoo.com>
2019-05-10 16:35:52 +0100

    Rovider and store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
-
+import { searchRobots } from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/> ,document.getElementById('root'));
+
+const store = createStore(searchRobots);
+
+
+ReactDOM.render(<Provider store={store}><App/></Provider> ,document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
