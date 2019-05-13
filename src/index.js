bc5675dfa8ab0c651773baa7ec019d0ac0d272ba
Vectormike40 <victor.junior40@yahoo.com>
2019-05-13 11:48:23 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ import * as serviceWorker from './serviceWorker';
 const store = createStore(searchRobots);
 
 
-ReactDOM.render(<Provider store={store}><App/></Provider> ,document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+                    <App/>
+                </Provider> ,document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
