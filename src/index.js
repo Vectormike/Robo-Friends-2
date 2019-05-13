030e228c4fd2581e44dc894c1a9d9f1b983b5bf0
Vectormike40 <victor.junior40@yahoo.com>
2019-05-13 12:53:19 +0100

    redux-logger

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './containers/App';
 import 'tachyons';
 import { searchRobots } from './reducers';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import * as serviceWorker from './serviceWorker';
 
-
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 
 ReactDOM.render(<Provider store={store}>
