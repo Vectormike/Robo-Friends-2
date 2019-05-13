7b75e36672e90e65ed0ef73771fd0d111fe78b8f
Vectormike40 <victor.junior40@yahoo.com>
2019-05-13 13:38:17 +0100

    request robots reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,16 +1,40 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import { 
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+ } from './constants'; 
 
-//Create initial state
-const initialState = {
+ //Create initial state
+const initialSearchState = {
     searchField: ''
 }
 
 // Reducer -->
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialSearchState, action={}) => {
     switch (action.type) {
         case CHANGE_SEARCH_FIELD:
             return Object.assign({}, state, {searchField: action.payload})
         default:
             return state;
+    }
+}
+
+const initialRobotsState = {
+    isPending: false,
+    robots: [],
+    error: ''
+}
+
+export const requestRobots = (state = initialRobotsState, action={}) => {
+    switch (action.type) {
+        case REQUEST_ROBOTS_PENDING:
+            return Object.assign({}, state, {isPending: true})
+        case REQUEST_ROBOTS_SUCCESS:
+            return Object.assign({}, state, {robots: action.payload, isPending: false})
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, state, {error: action.payload, isPending: false})
+        default:
+            return state
     }
 }
