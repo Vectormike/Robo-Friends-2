d6c4a921ae052a9f508673883847230040de4e4b
Vectormike40 <victor.junior40@yahoo.com>
2019-05-14 10:29:53 +0100

    Done

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,48 +3,46 @@ import { connect } from 'react-redux';
  import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
 import SearchBox from '../components/SearchBox';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 
 
 const mapStateToProps = (state) => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 }
 class App extends Component {
-    constructor() {
-        super()
-
-        this.state = {
-            robots: []
-        }
-    } 
     
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-
-        .then(response => response.json())  
-        .then((users) => {
-            this.setState({robots:users})
-        })
+        this.props.onRequestRobots()
     }
 
 
 
     render() {
-        const {searchField, onSearchChange} = this.props;
-        const filteredRobots = this.state.robots.filter(robot => {
+        const {searchField, onSearchChange, robots, isPending} = this.props;
+        const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField) 
         }) 
         
-        return (
+        return isPending ? 
+        <div className="tc">
+                <h1 className="f1 pa">RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <h1 className="tc pa3">Loading</h1> 
+        </div> :
+        (
             <div className="tc">
                 <h1 className="f1 pa">RoboFriends</h1>
                 <SearchBox searchChange={onSearchChange}/>
